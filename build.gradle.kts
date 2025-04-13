71e45c4b0e786fc0745f2e42451c8b08b7de4965
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-04-13 06:56:29 +0200

    add kotlinx DateTime

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.20"
 }
 
-group = "org.soulsquad"
+group = "org.seoulsquad"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
     testImplementation(kotlin("test"))
 }
 
