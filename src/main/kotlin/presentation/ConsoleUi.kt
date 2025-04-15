fc2a6d82cb05156a275c6c2edfea343d3433f6c9
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-04-15 15:59:13 +0200

    add showTenRandomPotatoMeals for the UI

--- src/main/kotlin/presentation/ConsoleUi.kt
+++ src/main/kotlin/presentation/ConsoleUi.kt
@@ -1,4 +1,36 @@
 package org.seoulsquad.presentation
 
-class ConsoleUi {
+import org.seoulsquad.logic.use_case.GetTenRandomPotatoMealsUseCase
+import org.seoulsquad.model.Meal
+import org.seoulsquad.presentation.utils.ConsoleColors
+import org.seoulsquad.presentation.utils.ConsoleStyle
+
+class ConsoleUi(
+    private val getTenRandomPotatoMealsUseCase: GetTenRandomPotatoMealsUseCase,
+) {
+    fun runApp() {
+        showTenRandomPotatoMeals()
+    }
+
+    private fun showTenRandomPotatoMeals() {
+        println("${ConsoleStyle.BOLD}${ConsoleColors.CYAN}This is very yummy, try this potato meals and Ed3yly.${ConsoleStyle.RESET}")
+
+        getTenRandomPotatoMealsUseCase().forEach {
+            printMeal(it)
+        }
+    }
+
+    private fun printMeal(meal: Meal) {
+        println(
+            """
+                -ID: ${meal.id}
+            This recipe is called: ${meal.name},
+            ${meal.description}
+            
+            Ingredients: ${meal.ingredients}
+            
+            ==============================================
+            """.trimIndent(),
+        )
+    }
 }
