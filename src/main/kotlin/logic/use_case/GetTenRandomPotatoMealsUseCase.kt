206d32548f3e97447b86dd6c128d00699c291bc4
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-04-15 15:57:12 +0200

    update logic

--- src/main/kotlin/logic/use_case/GetTenRandomPotatoMealsUseCase.kt
+++ src/main/kotlin/logic/use_case/GetTenRandomPotatoMealsUseCase.kt
@@ -3,7 +3,6 @@ package org.seoulsquad.logic.use_case
 import org.seoulsquad.logic.repository.MealRepository
 import org.seoulsquad.logic.utils.Constants.POTATO_ONLY
 import org.seoulsquad.model.Meal
-import kotlin.random.Random
 
 class GetTenRandomPotatoMealsUseCase(
     private val repository: MealRepository,
@@ -11,9 +10,7 @@ class GetTenRandomPotatoMealsUseCase(
     operator fun invoke(): List<Meal> {
         val mealsContainPotato = repository.getAllMeals().filter(::onlyWithPotato)
         return List(10) {
-            val randomMealIndex = Random.nextInt(until = mealsContainPotato.size)
-
-            mealsContainPotato[randomMealIndex]
+            mealsContainPotato.random()
         }
     }
 
