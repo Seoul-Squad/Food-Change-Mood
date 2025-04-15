622feb6d40658f6186b0bfe776cb23817e723c24
Mostafa Salah <mostafa.salah.569x@gmail.com>
2025-04-15 17:26:20 +0200

    add GetTenRandomEasyMealsUseCase to useCaseModule

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -1,8 +1,10 @@
 package org.seoulsquad.di
 
 import org.koin.dsl.module
+import org.seoulsquad.logic.use_case.GetTenRandomEasyMealsUseCase
 import org.seoulsquad.logic.use_case.GetAllMealsUseCase
 
 val useCaseModule= module {
     single { GetAllMealsUseCase(get()) }
+    single { GetTenRandomEasyMealsUseCase(get()) }
 }
