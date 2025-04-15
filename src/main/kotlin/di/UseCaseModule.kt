22322db4855a015f7af1bd5772ca82a5cedd5971
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-04-15 14:47:48 +0200

    update useCaseModule with GetTenRandomPotatoMealsUseCase

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -2,7 +2,10 @@ package org.seoulsquad.di
 
 import org.koin.dsl.module
 import org.seoulsquad.logic.use_case.GetAllMealsUseCase
+import org.seoulsquad.logic.use_case.GetTenRandomPotatoMealsUseCase
 
-val useCaseModule= module {
-    single { GetAllMealsUseCase(get()) }
-}
+val useCaseModule =
+    module {
+        single { GetAllMealsUseCase(get()) }
+        single { GetTenRandomPotatoMealsUseCase(get()) }
+    }
