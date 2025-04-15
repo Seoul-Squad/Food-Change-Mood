ed951158b5581f7138f47dbf810b1b2098960fbf
Mostafa Salah <mostafa.salah.569x@gmail.com>
2025-04-15 18:07:35 +0200

    add showTenRandomEasyMeals function to ConsoleUi

--- src/main/kotlin/presentation/ConsoleUi.kt
+++ src/main/kotlin/presentation/ConsoleUi.kt
@@ -1,4 +1,33 @@
 package org.seoulsquad.presentation
 
-class ConsoleUi {
+import data.model.Meal
+import org.seoulsquad.logic.use_case.GetTenRandomEasyMealsUseCase
+
+class ConsoleUi(
+    private val getTenRandomEasyMealsUseCase: GetTenRandomEasyMealsUseCase
+) {
+
+    private fun showTenRandomEasyMeals() {
+        getTenRandomEasyMealsUseCase().forEach { meal->
+            printMeal(meal)
+        }
+    }
+
+
+    private fun printMeal(meal: Meal) {
+        with(meal){
+            println("Meal: $name")
+            println("Prepare time: $minutes minutes")
+
+            println("Ingredients:")
+            ingredients.forEachIndexed { index, ingredient->
+                println("${index+1}- $ingredient")
+            }
+
+            println("Steps ($numberOfSteps):")
+            steps.forEachIndexed { index, step ->
+                println("${index+1}- $step")
+            }
+        }
+    }
 }
