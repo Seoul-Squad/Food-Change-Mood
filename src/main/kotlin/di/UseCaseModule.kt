38668ef0f635cad6040b7ce99b4c5b684f01bc92
marah1024 <maroohdroid@gmail.com>
2025-04-16 19:50:33 +0100

    Add Italian Large Group  resolve issues

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -1,13 +1,14 @@
 package di
 
+import logic.useCase.ExploreOtherCountriesFoodUseCase
+import org.koin.dsl.module
 import logic.useCase.GetAllMealsUseCase
 import logic.useCase.GetSweetsWithNoEggsUseCase
 import org.koin.dsl.module
-import org.seoulsquad.logic.useCase.GetItalianLargeMealsUseCase
 
 val useCaseModule =
     module {
         single { GetAllMealsUseCase(get()) }
         single { GetSweetsWithNoEggsUseCase(get()) }
-        single { GetItalianLargeMealsUseCase(get()) }
+        single { ExploreOtherCountriesFoodUseCase(get()) }
     }
