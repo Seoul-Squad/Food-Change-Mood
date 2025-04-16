8488d2c9e8579872dfddad21cfc936cffd5ee3db
Khaled.Eid <khaled.eid1k1k@gmail.com>
2025-04-16 09:41:06 +0200

    add use cases at di

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -1,11 +1,15 @@
-package di
+package org.seoulsquad.di
 
 import logic.useCase.GetAllMealsUseCase
 import logic.useCase.GetSweetsWithNoEggsUseCase
 import org.koin.dsl.module
+import org.seoulsquad.logic.useCase.GetMealUsingIDUseCase
+import org.seoulsquad.logic.useCase.SearchFoodsUsingDateUseCase
 
-val useCaseModule =
-    module {
-        single { GetAllMealsUseCase(get()) }
-        single { GetSweetsWithNoEggsUseCase(get()) }
-    }
+val useCaseModule = module {
+    single { GetAllMealsUseCase(get()) }
+    single { SearchFoodsUsingDateUseCase(get()) }
+    single { GetMealUsingIDUseCase(get()) }
+    single { GetSweetsWithNoEggsUseCase(get()) }
+
+}
