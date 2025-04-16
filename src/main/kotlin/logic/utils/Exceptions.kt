f17bf8de9b3fb8f1b5d2082a2232e35a111297b9
Khaled.Eid <khaled.eid1k1k@gmail.com>
2025-04-16 09:40:24 +0200

    add types of exception for invalid search and invalid id

--- src/main/kotlin/logic/utils/Exceptions.kt
+++ src/main/kotlin/logic/utils/Exceptions.kt
@@ -1,2 +1,11 @@
 package logic.utils
 
+
+open class InvalidDateException(message: String) : Exception(message)
+
+class InvalidDateForSearchException : InvalidDateException("Invalid date for search")
+
+class InvalidSearchException : InvalidDateException("No meals found")
+
+
+class InvalidId : Exception("Invalid id")
