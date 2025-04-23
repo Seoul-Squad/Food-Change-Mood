fdea8f5921db9eea0ac25ccab22cd980304bf161
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-04-23 07:42:58 +0200

    implement presentation layer dependency injection and inversion

---
+++ src/main/kotlin/presentation/consolelIO/Reader.kt
@@ -0,0 +1,13 @@
+package org.seoulsquad.presentation.consolelIO
+
+import kotlinx.datetime.LocalDate
+
+interface Reader {
+    fun readString(): String?
+
+    fun readInt(): Int?
+
+    fun readDouble(): Double?
+
+    fun readDate(): LocalDate?
+}
