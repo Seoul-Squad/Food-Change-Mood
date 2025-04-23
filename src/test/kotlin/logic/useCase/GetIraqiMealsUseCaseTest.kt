d0596375e4fc36f5a4c6662ceb6771961796fa06
Mohamed Hesham <medo93346@gmail.com>
2025-04-23 18:02:25 +0200

    refactor meal variable naming

--- src/test/kotlin/logic/useCase/GetIraqiMealsUseCaseTest.kt
+++ src/test/kotlin/logic/useCase/GetIraqiMealsUseCaseTest.kt
@@ -26,13 +26,13 @@ class GetIraqiMealsUseCaseTest{
     @Test
     fun `should return iraqi meals when tags only contains Iraq keyword in any format`() {
         // given
-        val iraqiMeals = listOf(
+        val meals = listOf(
             creatIraqiMeals(tags = listOf("mexico", "spicy"), description = "taco"),
             creatIraqiMeals(tags = listOf("iRaQ", "dinner", "spicy"), description = "traditional plate"),
             creatIraqiMeals(tags = listOf("iraq", "vegan"), description = "healthy option"),
             creatIraqiMeals(tags = listOf("Iraq", "weeknight", "course"), description = "this is an egyptian dish "),
         )
-        every { mealRepository.getAllMeals() } returns iraqiMeals
+        every { mealRepository.getAllMeals() } returns meals
         // when
         val result = getIraqiMealsUseCase()
         // then
@@ -43,13 +43,13 @@ class GetIraqiMealsUseCaseTest{
     @Test
     fun `should return iraqi meals when description only contains Iraq keyword in any format`() {
         // given
-        val iraqiMeals = listOf(
+        val meals = listOf(
             creatIraqiMeals(tags = listOf("italy", "pasta"), description = "traditional italian pasta"),
             creatIraqiMeals(tags = listOf("vegan"), description = "IrAq is known for this"),
             creatIraqiMeals(tags = listOf("dinner", "spicy"), description = "iraq special meal"),
             creatIraqiMeals(tags = listOf("lunch", "weeknight", "course"), description = "this is an Iraq dish "),
         )
-        every { mealRepository.getAllMeals() } returns iraqiMeals
+        every { mealRepository.getAllMeals() } returns meals
         // when
         val result = getIraqiMealsUseCase()
         // then
@@ -59,13 +59,13 @@ class GetIraqiMealsUseCaseTest{
     @Test
     fun `should return iraqi meals when both tags and description contain Iraq keyword in any case format`() {
         // given
-        val iraqiMeals = listOf(
+        val meals = listOf(
             creatIraqiMeals(tags = listOf("mexico"), description = "not related"),
             creatIraqiMeals(tags = listOf("iraq"), description = "iraq style"),
             creatIraqiMeals(tags = listOf("iRaQ", "weeknight", "course"), description = "this is an IrAq  dish "),
             creatIraqiMeals(tags = listOf("Iraq", "lunch"), description = "iraqi meal"),
         )
-        every { mealRepository.getAllMeals() } returns iraqiMeals
+        every { mealRepository.getAllMeals() } returns meals
         // when
         val result = getIraqiMealsUseCase()
         // then
@@ -76,12 +76,12 @@ class GetIraqiMealsUseCaseTest{
     @Test
     fun `should return iraqi meals when tags or description are empty or null`() {
         // given
-        val iraqiMeals = listOf(
+        val meals = listOf(
             creatIraqiMeals(tags = listOf("iraq"), description = null),
             creatIraqiMeals(tags = listOf(), description = "this is an iraqi dish "),
             creatIraqiMeals(tags = listOf("Iraq", "weeknight", "course"), description =""),
         )
-        every { mealRepository.getAllMeals() } returns iraqiMeals
+        every { mealRepository.getAllMeals() } returns meals
         // when
         val result = getIraqiMealsUseCase()
         // then
@@ -91,12 +91,12 @@ class GetIraqiMealsUseCaseTest{
     @Test
     fun `should return failure when no meals contain Iraq keyword in tags and description`() {
         // given
-        val iraqiMeals = listOf(
+        val meals = listOf(
             creatIraqiMeals(tags = listOf("Italy", "weeknight", "course"), description = "pizza"),
             creatIraqiMeals(tags = listOf("USA", "breakfast"), description = "eggs and bacon"),
             creatIraqiMeals(tags = listOf("Mexico", "weeknight", "course"), description = "this is an egyptian dish "),
         )
-        every { mealRepository.getAllMeals() } returns iraqiMeals
+        every { mealRepository.getAllMeals() } returns meals
         // when
         val result = getIraqiMealsUseCase()
         // then
@@ -106,13 +106,14 @@ class GetIraqiMealsUseCaseTest{
     @Test
     fun `should return failure when repository returns an empty list`() {
         // given
-        val iraqiMeals = emptyList<Meal>()
-        every { mealRepository.getAllMeals() } returns iraqiMeals
+        val meals = emptyList<Meal>()
+        every { mealRepository.getAllMeals() } returns meals
         // when
         val result = getIraqiMealsUseCase()
         // then
         assertThat(result.isFailure).isTrue()
         assertThat(result.exceptionOrNull()).isInstanceOf(NoMealsFoundException::class.java)
     }
+
 
 }
