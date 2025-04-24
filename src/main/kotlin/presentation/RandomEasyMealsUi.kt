83f55a05af5aa242d660d1b9645557706232ae4c
Mostafa Salah <mostafa.salah.569x@gmail.com>
2025-04-24 09:24:34 +0200

    add test cases of RandomEasyMealsUi

--- src/main/kotlin/presentation/RandomEasyMealsUi.kt
+++ src/main/kotlin/presentation/RandomEasyMealsUi.kt
@@ -1,10 +1,13 @@
 package org.seoulsquad.presentation
 
+import logic.model.Meal
 import logic.useCase.GetRandomEasyMealsUseCase
+import org.seoulsquad.presentation.consolelIO.Viewer
 import org.seoulsquad.presentation.utils.SharedUi
 
 class RandomEasyMealsUi(
-    private val getRandomEasyMealsUseCase: GetRandomEasyMealsUseCase
+    private val getRandomEasyMealsUseCase: GetRandomEasyMealsUseCase,
+    private val viewer: Viewer
 ) {
     fun printRandomEasyMeals() {
         val result = getRandomEasyMealsUseCase()
@@ -12,10 +15,37 @@ class RandomEasyMealsUi(
         result
             .onSuccess { randomEasyMealsList ->
                 randomEasyMealsList.forEach { meal ->
-                    SharedUi().printFullMeal(meal)
+                    printFullMeal(meal)
                 }
             }.onFailure { exception ->
-                println(exception.message)
+                //exception.message?.let { viewer.display(it) }
+                viewer.display(exception.message.toString())
             }
+    }
+
+
+    private fun printFullMeal(meal: Meal) {
+        with(meal) {
+            viewer.display("Meal: $name (ID: $id)")
+            viewer.display("Time to Prepare: $preparationTimeInMinutes minutes")
+            meal.description.takeIf { !it.isNullOrBlank() }?.run { viewer.display(this) }
+            viewer.display("Ingredients ($numberOfIngredients):")
+            ingredients.forEachIndexed { index, ingredient ->
+                viewer.display("  ${index + 1}. $ingredient")
+            }
+            viewer.display("Steps ($numberOfSteps):")
+            steps.forEachIndexed { index, step ->
+                viewer.display("  Step ${index + 1}: $step")
+            }
+            viewer.display("Nutrition:")
+            viewer.display("  - Calories: ${nutrition.calories} kcal")
+            viewer.display("  - Total Fat: ${nutrition.totalFat} g")
+            viewer.display("  - Saturated Fat: ${nutrition.saturatedFat} g")
+            viewer.display("  - Sugar: ${nutrition.sugar} g")
+            viewer.display("  - Sodium: ${nutrition.sodium} mg")
+            viewer.display("  - Protein: ${nutrition.protein} g")
+            viewer.display("  - Carbohydrates: ${nutrition.carbohydrates} g")
+        }
+        viewer.display("\n===========================================================\n")
     }
 }
---
+++ src/test/kotlin/presentation/RandomEasyMealsUiTest.kt
@@ -0,0 +1,68 @@
+package presentation
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import logic.useCase.GetRandomEasyMealsUseCase
+import logic.utils.NoMealsFoundException
+import mockData.createMeal
+import org.junit.jupiter.api.BeforeEach
+import org.seoulsquad.presentation.RandomEasyMealsUi
+import org.seoulsquad.presentation.consolelIO.Viewer
+import kotlin.test.Test
+
+class RandomEasyMealsUiTest {
+  private lateinit var getRandomEasyMealsUseCase: GetRandomEasyMealsUseCase
+  private lateinit var viewer: Viewer
+  private lateinit var randomEasyMealsUi: RandomEasyMealsUi
+
+  @BeforeEach
+  fun setup() {
+   getRandomEasyMealsUseCase = mockk()
+   viewer = mockk(relaxed = true)
+   randomEasyMealsUi = RandomEasyMealsUi(getRandomEasyMealsUseCase, viewer)
+  }
+
+    @Test
+    fun `should print random easy meals when use case return success result`() {
+        //Given
+        every { getRandomEasyMealsUseCase() } returns Result.success(meals)
+
+        //When
+        randomEasyMealsUi.printRandomEasyMeals()
+
+        //Then
+        verify { viewer.display(any()) }
+    }
+
+    @Test
+    fun `should print exception message when use case return failure result with exception`() {
+        //Given
+        every { getRandomEasyMealsUseCase() } returns Result.failure(NoMealsFoundException())
+
+        //When
+        randomEasyMealsUi.printRandomEasyMeals()
+
+        //Then
+        verify { viewer.display(NoMealsFoundException().message.toString()) }
+     }
+
+
+    companion object {
+        private val meals = listOf (
+            createMeal(1,25, 2, 5, null),
+            createMeal(2,15, 5, 3),
+            createMeal(3,18, 3, 4),
+            createMeal(4,28, 4, 5),
+            createMeal(5,30, 5, 6),
+            createMeal(6,25, 2, 5),
+            createMeal(7,15, 5, 3),
+            createMeal(8,18, 3, 4),
+            createMeal(9,28, 4, 5),
+            createMeal(10,30, 5, 6),
+            createMeal(11,30, 5, 6),
+            createMeal(12,30, 5, 6),
+        )
+    }
+
+ }
