d4cace8335530ba60295b8d04d285fe78d3f8039
Khaled.Eid <khaled.eid1k1k@gmail.com>
2025-04-24 17:32:34 +0200

    make SearchFoodsUsingDateUseCase testable - make ut receive local date instead of string - move validation of a date outside use case - make a test case for SearchFoodsUsingDateUseCase

--- src/main/kotlin/logic/useCase/SearchFoodsUsingDateUseCase.kt
+++ src/main/kotlin/logic/useCase/SearchFoodsUsingDateUseCase.kt
@@ -1,21 +1,23 @@
 package org.seoulsquad.logic.useCase
 
+import kotlinx.datetime.LocalDate
+import logic.utils.InvalidDateException
 import logic.utils.NoMealsFoundException
 import org.seoulsquad.logic.model.MealDate
 import org.seoulsquad.logic.repository.MealRepository
-import org.seoulsquad.logic.utils.convertDateToLocalDate
-import org.seoulsquad.logic.utils.paresDate
 
 
 class SearchFoodsUsingDateUseCase(
     private val repository: MealRepository
 ) {
-    operator fun invoke(data: String): Result<List<MealDate>> {
-        val date = convertDateToLocalDate(paresDate(data).getOrElse { return Result.failure(it) })
-        return repository.getAllMeals()
-            .filter { it.submittedAt == date }
-            .takeIf { it.isNotEmpty() }
-            ?.map { MealDate(id = it.id, nameOfMeal = it.name, date = it.submittedAt!!) }
-            ?.let { Result.success(it) } ?: Result.failure(NoMealsFoundException())
+    operator fun invoke(date: LocalDate?): Result<List<MealDate>> {
+        return date?.let {
+            repository.getAllMeals()
+                .filter { it.submittedAt == date }
+                .takeIf { it.isNotEmpty() }
+                ?.map { MealDate(id = it.id, nameOfMeal = it.name, date = it.submittedAt!!) }
+                ?.let { Result.success(it) } ?: Result.failure(NoMealsFoundException())
+        } ?: Result.failure(InvalidDateException())
+
     }
 }
--- src/main/kotlin/logic/utils/Extensions.kt
+++ src/main/kotlin/logic/utils/Extensions.kt
@@ -7,6 +7,6 @@ fun List<Double>.veryLowNutritionValue(percentage: Double): Double {
     val index = (percentage * (this.size - 1)).toInt()
     return this[index]
 }
-fun List<MealDate>.isIdExistingAtList(id: String): Boolean {
-    return any { it.id == id.toInt() }
+fun List<MealDate>.isIdExistingAtList(id: Int): Boolean {
+    return any { it.id == id }
 }
