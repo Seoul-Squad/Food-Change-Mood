74a04dd344bdac6cc21e536510fe4332ad342e8c
edrees <muhammed.edrees101@gmail.com>
2025-04-28 08:05:55 +0300

    some commit

--- src/main/kotlin/presentation/consolelIO/ConsoleReader.kt
+++ src/main/kotlin/presentation/consolelIO/ConsoleReader.kt
@@ -3,9 +3,9 @@ package org.seoulsquad.presentation.consolelIO
 class ConsoleReader : Reader {
     override fun readString(): String = readln()
 
-    override fun readInt(): Int? = readlnOrNull()?.toIntOrNull()
+    override fun readInt(): Int? = readln().toIntOrNull()
 
-    override fun readDouble(): Double? = readlnOrNull()?.toDoubleOrNull()
+    override fun readDouble(): Double? = readln().toDoubleOrNull()
 
 }
 
